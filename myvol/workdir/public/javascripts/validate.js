6cb290d9981a79825789ef069f82f9f8ef6f8e61
Masaya Yamada <yamada@kde.cs.tsukuba.ac.jp>
2020-05-12 22:24:48 +0900

    エラー出力関数を統一

--- myvol/workdir/public/javascripts/validate.js
+++ myvol/workdir/public/javascripts/validate.js
@@ -1,5 +1,11 @@
 'use strict';
 
+function deleteErr() {
+  $('#nullErr').empty();
+  $('#startErr').empty();
+  $('#endErr').empty();
+}
+
 $("#form").submit(function () {
   console.log('a');
   let err = false;
@@ -13,31 +19,25 @@ $("#form").submit(function () {
   const end = new Date(endDate + ' ' + endTime + ':' + endMin + ':00');
   const is_null = (!startDate || !startTime || !startMin || !endDate || !endTime || !endMin);
 
+  deleteErr();
+
   // 空値あり
   if (is_null) {
     err = true;
-    $('#startErr').empty();
-    $('#endErr').empty();
-    $('#nullErr').empty();
     $('#nullErr').append('<p>全ての要素を入力してください</p>');
   }
   else {
-    $('#nullErr').empty();
     // 開始時間が現在時間より前
     if (start < new Date()) {
       err = true;
-      $('#startErr').empty();
       $('#startErr').append('<p>開始時間は現在時刻より後を指定してください</p>');
     }
     // 終了日時が開始時間より前
     if (end <= start) {
       err = true;
-      $('#endErr').empty();
       $('#endErr').append('<p>終了時間は開始時間より後を指定してください</p>');
     }
   }
   if (err)
     return false;
-
 });
-// $('#err').text("out");
