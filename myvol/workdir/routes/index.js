ae20406e2bece6797639020a1520fdfa2c9da12d
Masaya Yamada <yamada@kde.cs.tsukuba.ac.jp>
2020-06-02 20:41:24 +0900

    デバッグ用コードの削除

--- myvol/workdir/routes/index.js
+++ myvol/workdir/routes/index.js
@@ -10,11 +10,6 @@ const title = require('./title');
 /* GET home page. */
 router.get('/', csrfProtection, function (req, res, next) {
   // console.log('::user::' + JSON.stringify(req.user));
-  // Users.bulkCreate([
-  //   { name: 'admin', password: '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918' },
-  //   { name: 'user1', password: '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' },
-  //   { name: 'user2', password: '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' }
-  // ]).then(() => { });
   if (req.user) {
     Items.findAll({
       attributes: ['id', 'name', 'comment', 'createdBy'],
